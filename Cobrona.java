1a8dc2361c130a3ebcf9ddac8b8de69ad0da80cc
joaovcandrade <jvcostaandrade7399@gmail.com>
2019-05-13 22:07:53 -0300

    Configuração inicial

--- Cobrona.java
+++ Cobrona.java
@@ -14,5 +14,24 @@ public class Cobrona extends Actor
      */
     public void act()
     {
+        move(2);
+        if(Greenfoot.isKeyDown("left")){
+            if(this.getRotation() != 180 && this.getRotation() != 0){
+                setRotation(180);
+            }
+        } else if(Greenfoot.isKeyDown("right")){
+            if(this.getRotation() != 180 && this.getRotation() != 0){
+                setRotation(0);
+            }
+        } else if(Greenfoot.isKeyDown("down")){
+            if(this.getRotation() != 90 && this.getRotation() != 270){
+                setRotation(90);
+            }
+        } else if(Greenfoot.isKeyDown("up")){
+            
+            if(this.getRotation() != 90 && this.getRotation() != 270){
+            setRotation(270);
+            }
+        }
     }
 }
--- Deserto.java
+++ Deserto.java
@@ -16,6 +16,8 @@ public class Deserto extends World
     public Deserto()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        Cobrona snake = new Cobrona();
+        
     }
 }
