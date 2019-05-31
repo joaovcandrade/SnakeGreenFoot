38559b53c6f05e177de4f224ec5f5310c3825317
João Vitor <jvcostaandrade7399@gmail.com>
2019-05-31 20:53:48 -0300

    worldSnake

---
+++ worldSnake.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class worldSnake extends World
+{    
+    Fila fila;
+    
+    public worldSnake()
+    {    
+        //Malha de fundo 
+        super(30, 30, 20); 
+        GreenfootImage img = new GreenfootImage(20,20);
+        img.drawRect(0,0,20,20);
+        setBackground(img);
+        
+        //Coloca a cabra
+        fila = new Fila();        
+        addObject(new Cobrona(), 10, 10);
+    }    
+}
